0f7df3e330b019b511c68255d288a25356ebb08f
BadiGamer <48218842+BadiGamer@users.noreply.github.com>
2019-05-03 02:09:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2134,7 +2134,19 @@ message.author.sendEmbed(embed)
 }
 });
 
-
+client.on('message', function(message) {
+    if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        var stewart = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTimestamp()
+            .setTitle('``رساله جديده في خاص البوت``')
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+            .setFooter(`من (@${message.author.tag})  |  (${message.author.id})`)
+        client.channels.get("573303661334233099").send({ embed: stewart });
+    }
+});
 
 
 
